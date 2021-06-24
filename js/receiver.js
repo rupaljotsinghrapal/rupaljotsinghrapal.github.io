28e4d79520b1ccfac12a86d396c3a95f990e76ef
TheHalcyon <hushpuppies1993@TheHalcyons-MacBook-Air.local>
2021-06-24 15:11:37 +0530

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -171,7 +171,7 @@ playerManager.setMessageInterceptor(
   cast.framework.messages.MessageType.LOAD, loadRequestData => {
     castDebugLogger.error(LOG_RECEIVER_TAG,
       `LOAD interceptor loadRequestData: ${JSON.stringify(loadRequestData)}`);
-    document.getElementById("heading").innerHTML = window.navigator.userAgent;
+    document.getElementById("heading").innerHTML = window.navigator.appCodeName;
     document.getElementById("heading").style.fontSize = 20
 
     
